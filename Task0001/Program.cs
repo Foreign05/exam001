a627592d92e908054d0fd4da42725b8a82b5aa79
Legion <nonamef4ke@gmail.com>
2023-08-19 15:03:59 +0500

    inputSplittedToArray

--- Task0001/Program.cs
+++ Task0001/Program.cs
@@ -5,6 +5,8 @@ class Program
     {
         Console.WriteLine("Enter the elements of the array: ");
         string input = Console.ReadLine(); //get input 
+        string[] elements = input.Split(' '); //store input values separated in massive 
 
+        int size = elements.Length;  //analyze length of massive
     }
 }
