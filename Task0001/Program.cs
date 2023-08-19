39433c27e1b72484fd04dd6025b103ead21881ef
Legion <nonamef4ke@gmail.com>
2023-08-19 15:05:43 +0500

    printLoopCreated

--- Task0001/Program.cs
+++ Task0001/Program.cs
@@ -8,5 +8,14 @@ class Program
         string[] elements = input.Split(' '); //store input values separated in massive 
 
         int size = elements.Length;  //analyze length of massive
+
+        Console.WriteLine("The expected array is:");
+
+        foreach (string element in elements) //printing whole massive
+        {
+        
+           Console.Write(element + " "); //simple print
+        
+        }
     }
 }
