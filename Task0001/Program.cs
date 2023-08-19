29adb58fcc778be80989fa320b80b00dfd5d0bd6
Legion <nonamef4ke@gmail.com>
2023-08-19 14:43:28 +0500

    projectFirstCommit

---
+++ Task0001/Program.cs
@@ -0,0 +1,10 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the elements of the array: ");
+        string input = Console.ReadLine(); //get input 
+
+    }
+}
